9fbfbb860c3d3e752fdbc82638b76bc8dba772af
Jack Twilley <mathuin@gmail.com>
2015-12-29 15:15:05 -0800

    Added python shell prompts and reformatted file.

--- emacs/.emacs.d/settings/my-customize.el
+++ emacs/.emacs.d/settings/my-customize.el
@@ -9,7 +9,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((python-shell-interpreter-args . "manage.py shell") (python-shell-interpreter . "python")))))
+ '(safe-local-variable-values
+   (quote
+    ((python-shell-prompt-output-regexp . "Out\\[[0-9]+\\]: ")
+     (python-shell-prompt-regexp . "In \\[[0-9]+\\]: ")
+     (python-shell-interpreter-args . "manage.py shell")
+     (python-shell-interpreter . "python")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
